e18f5c246f0accf3a3e3cc0c91f517447b269047
victorhez <Victorhezishere@gmail.com>
2021-02-26 00:36:18 +0100

    basic setup

---
+++ bookings/lib/app/modules/home/bindings/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:bookings/app/modules/home/controllers/home_controller.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
